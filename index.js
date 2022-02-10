95b45f0eb127c5076f4170216cf75d552a1fbf94
Milyakova <versuchi@yandex.ru>
2022-02-10 15:35:05 +0300

    pure Node.js server without frameworks

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const http=require('http')
 const chalk=require('chalk')
 const port=3000
-const server=http.createServer((req,res)=>{
-    console.log('request method ', req.method)
-    console.log('request url ', req.url)
-    res.end('Hello from server, response was ended')
+const fs=require('fs/promises')
+const path=require('path')
+const basePath=path.join(__dirname,'pages')
+const {addNote}=require('./notes.controller')
+
+const server=http.createServer(async(req,res)=>{
+    if (req.method==='GET'){
+        const content=await fs.readFile(path.join(basePath, 'index.html'))
+        // console.log(content.toString('utf-8'))
+        // res.setHeader('Content-Type', 'text/html')
+        // res.setHeader('Content-Type', 'text/plain') html-верстка
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.end(content)
+    }else if (req.method==='POST'){
+        const body=[]
+        res.writeHead(200,{
+            'Content-Type':'text/plain; charset=utf-8'
+        })
+        req.on('data',data=>{
+            body.push(Buffer.from(data))
+        })
+        req.on('end',()=>{
+            const title=body.toString().split('=')[1].replaceAll('+',' ')
+            addNote(title)
+            res.end(`Post success with title=${title}`)
+        })
+
+    }
+
 })
 server.listen(port,()=>{
     console.log(chalk.green(`Server has been started on port ${port}`))
