8c80a22aafabdd2eeabd73def49b1edee43bff8b
Milyakova <versuchi@yandex.ru>
2022-02-10 13:57:44 +0300

    Server has been started on port

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-const yargs=require('yargs')
-const {addNote,removeNote, printNotes}=require('./notes.controller')
-
-yargs.command({
-    command:"add",
-    describe:'add new note to list',
-    builder:{
-        title:{
-            type:'string',
-            describe:'Note title',
-            demandOption:true
-        }
-    },
-    async handler({title}){
-       await addNote(title)
-    }
+const http=require('http')
+const chalk=require('chalk')
+const port=3000
+const server=http.createServer((req,res)=>{
+    console.log('request method ', req.method)
+    console.log('request url ', req.url)
+    res.end('Hello from server, response was ended')
 })
-yargs.command({
-    command:"remove",
-    describe:'remove note from list',
-    builder:{
-        id:{
-            type:'string',
-            describe:'Note id',
-            demandOption:true
-        }
-    },
-    async handler({id}){
-        await removeNote(id)
-    }
+server.listen(port,()=>{
+    console.log(chalk.green(`Server has been started on port ${port}`))
 })
-
-
-yargs.command({
-    command:"list",
-    describe:'print all notes',
-    async handler(){
-       await printNotes()
-
-    }
-})
-
-yargs.parse()
